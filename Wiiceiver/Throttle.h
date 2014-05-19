49b36da3903f29111e3c69776adf93fa9ecdaeb7
Austin David <austin@austindavid.com>
2014-05-19 00:07:45 -0700

    don't smooth brakes
    braking (and releasing) is now unsmoothed, totally responsive.

--- Wiiceiver/Throttle.h
+++ Wiiceiver/Throttle.h
@@ -76,102 +76,6 @@ class Throttle {
 #endif         
     }
     
-    
-  public:    
-    
-    Throttle() {
-      smoother = Smoother();
-      throttle = 0;
-      autoCruise = THROTTLE_MIN_CC;
-      xCounter = 0;
-    } // Throttle()
-    
-
-    void init(void) {
-      readAutoCruise();
-    }
-    
-    /*
-     * returns a smoothed float [-1 .. 1]
-     *
-     * Theory of Operation: identify the throttle position (joystick angle), 
-     *   then return a smoothed representation
-     *
-     *   if C is pressed, "cruise control":
-     *      set "cruise" to last joystick position
-     *      if joystick == up, increment throttle position (Z button: 3x increment)
-     *      if joystick == down, decrement throttle position  (Z button: 3x decrement)
-     *   else throttle position == chuck.Y joystick position
-     *   return a smoothed value from the throttle position (Z button: 4x less smoothed)
-     */
-    float update(Chuck chuck) {
-#ifdef DEBUGGING_THROTTLE
-      Serial.print("Throttle: ");
-      Serial.print("y=");
-      Serial.print(chuck.Y, 4);
-      Serial.print(", ");
-      Serial.print("c=");
-      Serial.print(chuck.C);
-      Serial.print("; ");
-#endif
-
-      // we're looking for autoCruise, so do that;
-      // don't change the throttle position
-      if (checkAutoCruise(chuck)) {
-        return throttle; 
-      }
-      
-
-      // CC return: in CC mode, drop C, then resume shortly after 
-      // (with no other input) -- resume the previous CC 
-      if (checkCruiseReturn(chuck)) {
-#ifdef DEBUGGING_THROTTLE
-        Serial.print(" returning previous cruise level");
-#endif         
-        throttle = previousCruiseLevel;
-      }  
-      
-      if (chuck.C) { // cruise control!
-        // holding Z + C == faster CC changes
-        if (chuck.Y > 0.5 && throttle < 1.0) {
-          throttle += THROTTLE_CC_BUMP * (chuck.Z ? 2 : 1);
-        } else {
-          if (chuck.Y < -0.5 && throttle > 0) {
-            throttle -= THROTTLE_CC_BUMP * (chuck.Z ? 2 : 1);
-          } else {
-            if (throttle < autoCruise) {
-              throttle += THROTTLE_CC_BUMP * (chuck.Z ? 4 : 2);
-            }
-          }
-        } // if (chuck.Y > 0.5 && throttle < 1.0) - else
-      } else { // if (checkAutoCruise() && chuck.C)
-        throttle = chuck.Y;
-        // "center" the joystick by enforcing some very small minimum
-        if (abs(throttle) < THROTTLE_MIN) {
-          throttle = 0;
-        }
-      } // if (chuck.C) -- else
-
-      // if formerly braking, just go to idle (don't smooth the brake release)
-      if (abs(chuck.Y) < THROTTLE_MIN &&
-          smoothed < 0) {
-        zero();
-      } else {
-        // holding Z == more aggressive smoothing (e.g., more responsive)
-        smoothed = smoother.compute(throttle, THROTTLE_SMOOTHNESS * (chuck.Z ? 4 : 1));
-      }
-      
-#ifdef DEBUGGING_THROTTLE
-      Serial.print(F("position: "));
-      Serial.print(throttle, 4);
-      Serial.print(F(" smoothed: "));
-      Serial.println(smoothed, 4);
-#endif
-
-      return smoothed;
-    } // float update(void)
-    
-    
     /*
      * returns 'true' if we're in a "set autocruise level" state:
      *   while in cruise (chuck.C), with no throttle input (chuck.Y =~ 0), 
@@ -220,19 +124,8 @@ class Throttle {
       autoCruise = throttle;
       writeAutoCruise();
     } // setAutoCruise(void)
-    
-    
-    float getThrottle(void) {
-      return smoothed;
-    } // float getThrottle()
-    
-    
-    void zero(void) {
-      throttle = smoothed = 0;
-      smoother.zero();
-    } // void zero(void)
-    
-    
+
+
     /*
      * true if we should return to the previous CC value
      * ... because we were holding C for a bit, then stopped (coasted),
@@ -283,6 +176,111 @@ class Throttle {
       previousC = chuck.C;
       return retval;
     } // checkCruiseReturn(Chuck chuck)
+
+    
+  public:    
+    
+    Throttle() {
+      smoother = Smoother();
+      throttle = 0;
+      autoCruise = THROTTLE_MIN_CC;
+      xCounter = 0;
+    } // Throttle()
+    
+
+    void init(void) {
+      readAutoCruise();
+    }
+    
+    /*
+     * returns a smoothed float [-1 .. 1]
+     *
+     * Theory of Operation: identify the throttle position (joystick angle), 
+     *   then return a smoothed representation
+     *
+     *   if C is pressed, "cruise control":
+     *      set "cruise" to last joystick position
+     *      if joystick == up, increment throttle position (Z button: 3x increment)
+     *      if joystick == down, decrement throttle position  (Z button: 3x decrement)
+     *   else throttle position == chuck.Y joystick position
+     *   return a smoothed value from the throttle position (Z button: 4x less smoothed)
+     */
+    float update(Chuck chuck) {
+#ifdef DEBUGGING_THROTTLE
+      Serial.print("Throttle: ");
+      Serial.print("y=");
+      Serial.print(chuck.Y, 4);
+      Serial.print(", ");
+      Serial.print("c=");
+      Serial.print(chuck.C);
+      Serial.print("; ");
+#endif
+
+      // we're looking for autoCruise, so do that;
+      // don't change the throttle position
+      if (checkAutoCruise(chuck)) {
+        return throttle; 
+      }
+
+      // CC return: in CC mode, drop C, then resume shortly after 
+      // (with no other input) -- resume the previous CC 
+      if (checkCruiseReturn(chuck)) {
+#ifdef DEBUGGING_THROTTLE
+        Serial.print(" returning previous cruise level");
+#endif         
+        throttle = previousCruiseLevel;
+      }  
+      
+      if (chuck.C) { // cruise control!
+        // holding Z + C == faster CC changes
+        if (chuck.Y > 0.5 && throttle < 1.0) {
+          throttle += THROTTLE_CC_BUMP * (chuck.Z ? 2 : 1);
+        } else {
+          if (chuck.Y < -0.5 && throttle > 0) {
+            throttle -= THROTTLE_CC_BUMP * (chuck.Z ? 2 : 1);
+          } else {
+            if (throttle < autoCruise) {
+              throttle += THROTTLE_CC_BUMP * (chuck.Z ? 4 : 2);
+            }
+          }
+        } // if (chuck.Y > 0.5 && throttle < 1.0) - else
+      } else { // if (checkAutoCruise() && chuck.C)
+        throttle = chuck.Y;
+        // "center" the joystick by enforcing some very small minimum
+        if (abs(throttle) < THROTTLE_MIN) {
+          throttle = 0;
+        }
+      } // if (chuck.C) -- else
+
+      // don't smooth brakes
+      if (throttle <= 0) {
+        smoothed = smoother.compute(throttle, 1.0);
+      } else {
+        // holding Z == more aggressive smoothing (e.g., more responsive)
+        smoothed = smoother.compute(throttle, THROTTLE_SMOOTHNESS * (chuck.Z ? 4 : 1));
+      }
+      
+#ifdef DEBUGGING_THROTTLE
+      Serial.print(F("position: "));
+      Serial.print(throttle, 4);
+      Serial.print(F(" smoothed: "));
+      Serial.println(smoothed, 4);
+#endif
+
+      return smoothed;
+    } // float update(void)
+    
+    
+    float getThrottle(void) {
+      return smoothed;
+    } // float getThrottle()
+    
+    
+    void zero(void) {
+      throttle = smoothed = 0;
+      smoother.zero();
+    } // void zero(void)
+
     
 }; // class Throttle
 
